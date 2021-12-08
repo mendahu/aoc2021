5f6a7eefc2ec8080d6c2b83d5b851ef8cae4406e
Jake Robins <me@jakerobins.com>
2021-12-07 19:31:03 -0600

    add ingestion function to bingo

--- BingoValidator/BingoValidator.rb
+++ BingoValidator/BingoValidator.rb
@@ -1,4 +1,44 @@
 require './Utilities/DataLoader.rb'
 
+class Board
+  def initialize(tiles)
+    @board = tiles
+    @pulls = []
+  end
+
+  def record_pull(num)
+    @pulls.push(num)
+  end
+end
+
 class BingoValidator < DataLoader
+  def initialize(path)
+    super
+
+    @pulls = get_pulls(@@data)
+    @boards = get_boards(@@data)
+  end
+
+  def get_pulls(data)
+    data[0].split(",")
+  end
+
+  def get_boards(data)
+    board_data = data[2...-1]
+    boards = []    
+    index = 0
+    
+    while index < board_data.length
+      board_array = [0...5].map.with_index { |x, i| board_data[index + i].split(" ")}
+      board = Board.new(board_array)
+      boards.push(board)
+      index += 6
+    end
+
+    return boards
+  end
+
+  def validate
+  
+  end
 end
